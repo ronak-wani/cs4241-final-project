892698f8961fb4c174f47efd1e0b3309ad9330c3
Ronak Wani <ronak_wani@outlook.com>
2024-04-14 16:06:44 -0400

    MongoDB setup completed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import example from "./routes/example";
+import connectionDB from './routes/dbConnection'
 
 const app = express();
-
 app.use(express.json());
-
+connectionDB;
 app.use("/api/example", example);
 
-app.listen(3001, () => {
+app.listen(5000, () => {
     console.log("started");
 });
 
