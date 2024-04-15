5a27735988111030b636a890dc666155dd10064e
Gabriel Shiu <gabriel@shiu.us>
2024-04-15 15:08:46 -0400

    refactor: simplify tile values state, default cursor on flipped or done tiles, matching refactor

--- frontend/src/routes/Memory.tsx
+++ frontend/src/routes/Memory.tsx
@@ -1,26 +1,8 @@
 import React, { useState } from 'react';
 import '../css/Memory.css';
 
-// interface for the tile object in the game
-interface tileData {
-    tileID: number;
-    tileValue: string;
-}
-
-// added 12 data values since we're dealing with a 4 x 3 grid, so 6 pairs total
-const tileDataValues: tileData[] = [
-    { tileID: 0, tileValue: 'Blue' },
-    { tileID: 1, tileValue: 'Green' },
-    { tileID: 2, tileValue: 'Red' },
-    { tileID: 3, tileValue: 'Blue' },
-    { tileID: 4, tileValue: 'Green' },
-    { tileID: 5, tileValue: 'Red' },
-    { tileID: 6, tileValue: 'Yellow' },
-    { tileID: 7, tileValue: 'Yellow' },
-    { tileID: 8, tileValue: 'Purple' },
-    { tileID: 9, tileValue: 'Purple' },
-    { tileID: 10, tileValue: 'Orange' },
-    { tileID: 11, tileValue: 'Orange' },
+const values = [
+    'Blue', 'Green', 'Red', 'Blue', 'Green', 'Red', 'Yellow', 'Yellow', 'Purple', 'Purple', 'Orange', 'Orange'
 ];
 
 // when interacting with the tile object, these are the props that it accepts
@@ -46,7 +28,7 @@ function Tile({ value, onClick, isFlipped, isDone }: tileProps) {
 function Memory() {
 
     // useState hooks to manage state of tiles and IDs of flipped ones
-    const [tiles] = useState<tileData[]>(tileDataValues);
+    const [tiles] = useState<string[]>(values);
     const [flippedTiles, setFlippedTiles] = useState<number[]>([]);
     const [doneTiles, setDoneTiles] = useState<number[]>([]);
 
@@ -62,16 +44,13 @@ function Memory() {
 
         if (flippedTiles.length === 1) {
             // if two tiles have just been flipped
-
-            if (tiles[flippedTiles[0]].tileValue === tiles[tileID].tileValue) {
-                // if the two tiles match, add them to the doneTiles array
-                setTimeout(() => {
-                    setDoneTiles([...doneTiles, flippedTiles[0], tileID]);
-                }, 1000);
-            }
             // set a timer to hide them
             setTimeout(() => {
                 setFlippedTiles([]);
+                if (tiles[flippedTiles[0]] === tiles[tileID]) {
+                    // if the two tiles match, add them to the doneTiles array
+                    setDoneTiles([...doneTiles, flippedTiles[0], tileID]);
+                }
             }, 1000);
         }
 
@@ -82,13 +61,13 @@ function Memory() {
     // renders tile component for each tile in the game
     return (
         <div className="memory_game_board">
-            {tiles.map(tile => (
+            {tiles.map((value, index) => (
                 <Tile
-                    key={tile.tileID}
-                    value={tile.tileValue}
-                    isFlipped={flippedTiles.includes(tile.tileID)}
-                    isDone={doneTiles.includes(tile.tileID)}
-                    onClick={() => handleTileClick(tile.tileID)}
+                    key={index}
+                    value={value}
+                    isFlipped={flippedTiles.includes(index)}
+                    isDone={doneTiles.includes(index)}
+                    onClick={() => handleTileClick(index)}
                 />
             ))}
         </div>
