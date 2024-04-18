dd6186be5699d9fa26a1170ce991529c78e62d6c
TheLegacy-Coder <klaudiofushakako@gmail.com>
2024-04-18 14:35:31 -0400

    Working project-styling base branch

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 function NavBar() {
     return (
-        <div className={"h-16 flex px-6 items-center w-screen bg-emerald-200 gap-4"}>
-            <a className="font-bold text-green-950-600 hover:text-blue-800 visited:text-darkgreen-600"
+        <div style={{
+            backgroundImage: "url('frontend/public/logo512.png')",
+            backgroundSize: 'cover',
+        }}
+             className={"h-16 flex px-6 items-center w-screen bg-emerald-200 gap-4"}>
+            <a className="font-bold text-green-950 hover:text-green-600"
                href={"/"}>Homepage</a>
-            <a href={"/login"}>Login</a>
-            <a href={"/leaderboard"}>Leaderboard</a>
-            <a href={"/memory"}>Memory</a>
-            <a href={"/example"}>Example</a>
+            <a className="font-bold text-green-950 hover:text-green-600"
+               href={"/login"}>Login</a>
+            <a className="font-bold text-green-950 hover:text-green-600"
+               href={"/leaderboard"}>Leaderboard</a>
+            <a className="font-bold text-green-950 hover:text-green-600"
+               href={"/memory"}>Memory</a>
+            <a className="font-bold text-green-950 hover:text-green-600"
+               href={"/example"}>Example</a>
         </div>
     );
 }
