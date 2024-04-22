6defe7d710419c35580f47b4bbb7bd4257b6b6e4
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-04-22 13:07:37 -0400

    set backend auth file to use the api path

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -16,7 +16,7 @@ function Login() {
         const urlParams = new URLSearchParams(queryString);
         const codeParams = urlParams.get("code");
         const getUserData = async () => {
-            const response = await fetch("http://localhost:5000/getUserData", {
+            const response = await fetch("api/auth/getUserData", {
                 method: "GET",
                 headers: {
                     "Authorization": "Bearer " + localStorage.getItem("accessToken"),
@@ -31,7 +31,7 @@ function Login() {
             const getAccessToken = async () => {
                 console.log("inside access Token");
                 try {
-                    const response = await axios.get("http://localhost:5000/getAccessToken?code=" + codeParams, {
+                    const response = await axios.get("api/auth/getAccessToken?code=" + codeParams, {
                         headers: {
                             mode: "no-cors",
                             credentials: "include"
@@ -95,7 +95,7 @@ function Login() {
                         }}>Logout
                         </button>
                         <button className="bg-green-950 hover:bg-emerald-300 text-white font-bold py-2 px-4 rounded-full"
-                                style={{width: "30%"}} onClick={() => window.location.href = "http://localhost:3000/memory"}>Go Play</button>
+                                style={{width: "30%"}} onClick={() => window.location.href = "/memory"}>Go Play</button>
 
                         <h2 className="font-bold text-white">User Data: </h2>
                         {/*<button onClick={getUserData}>Get Data</button>*/}
