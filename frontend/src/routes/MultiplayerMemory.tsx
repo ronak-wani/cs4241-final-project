809df27063d79ac919be24cd392fe70f4fda9f81
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-04-25 13:04:09 -0400

    Turns are implemented and the winner is determined. The minimum necessary functionality for multiplayer is achieved.

--- frontend/src/routes/MultiplayerMemory.tsx
+++ frontend/src/routes/MultiplayerMemory.tsx
@@ -1,14 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import '../css/Memory.css';
-import axios from 'axios';
 
-const ws = new WebSocket( 'ws://localhost:5000' );
+const ws = new WebSocket( 'ws://localhost:5000/api' );
 // when interacting with the tile object, these are the props that it accepts
 interface tileProps {
     value: string;
     onClick: () => void;
     isFlipped: boolean;
-    isDone: boolean;
+    isDone: number;
 }
 
 // represents an individual tile component
@@ -16,15 +15,16 @@ interface tileProps {
 // accepts props for value and an onClick handler
 function Tile({ value, onClick, isFlipped, isDone }: tileProps) {
     let statusClass = 'bg-green-500 cursor-pointer';
-    if (isDone) {
+    if (isDone === 1) {
         statusClass = 'bg-blue-200 cursor-default';
-    }
-    else if (isFlipped) {
+    } else if (isDone === -1) {
+        statusClass = 'bg-red-200 cursor-default';
+    } else if (isFlipped) {
         statusClass = 'bg-green-200 cursor-default';
     }
 
-    let content = '';
-    if (isDone) {
+    let content: string;
+    if (isDone === 1 || isDone === -1) {
         content = '✓';
     }
     else if (isFlipped) {
@@ -56,7 +56,6 @@ function MultiplayerMemory() {
         return shuffledValues.map((value) => String(value));
     }
 
-    const [difficulty, setDifficulty] = useState<string>('easy');
     const [rows, setRows] = useState<number>(3);
     const [cols, setCols] = useState<number>(4);
     const [tiles, setTiles] = useState<string[]>([]);
@@ -67,34 +66,15 @@ function MultiplayerMemory() {
     const [startTime, setStartTime] = useState<number>(0);
     const [intervalID, setIntervalID] = useState<NodeJS.Timer>();
 
+    const [turn, setTurnn] = useState<number>(0);
+    function setTurn(index:number) {
+        console.log("set Turn to " + index);
+        setTurnn(index)
+    }
+
     // states: idle, play, won
     const [state, setState] = useState<string>('idle');
-    const [username, setUsername] = useState<string>('');
 
-    useEffect(() => {
-        const getUserData = async () => {
-            const response = await fetch("/api/auth/getUserData", {
-                method: "GET",
-                headers: {
-                    "Authorization": "Bearer " + localStorage.getItem("accessToken"),
-                },
-            });
-            const data = await response.json();
-            setUsername(data.login);
-        };
-        getUserData();
-    }, []);
-
-    const handleWon = async () => {
-        console.log(time, (new Date()).getTime(), username);
-        const data = {
-            username: username,
-            score: time,
-            game: 'memory-' + difficulty,
-        }
-        console.log(data);
-        const response = await axios.post("/api/dbScoreRoutes", data)
-    }
 
     useEffect(() => {
         if (state !== 'play') return;
@@ -110,23 +90,31 @@ function MultiplayerMemory() {
     useEffect(() => {
         if (state === 'won') {
             clearInterval(intervalID);
-            alert("You won! Time: " + msToReadable(time));
+            let score = 0
+            matched.forEach((m) => {score += m;});
+            if (score > 0) {
+                alert("Congratulations, you won! You now have bragging rights.");
+            } else if (score < 0) {
+                alert("Unfortunately, you lost. Better look next time.");
+            } else {
+                alert("You tied. Lets count this as a win.");
+            }
             setState('idle');
-
-            handleWon();
+            setTurn(0);
         }
     }, [state])
 
     function StartGame() {
+        setTurn(2);
         setState('play');
         setTiles(createTiles(rows, cols));
         setFlipped([]);
-        setMatched([]);
+        setMatched(Array<number>(cols*rows).fill(0));
         setStartTime((new Date()).getTime());
     }
 
     // function that handles the tile flipping and matching logic
-    const handleTileFlip= (tileID: number) => {
+    const handleTileFlip= (tileID: number, index: number) => {
         console.log(state)
         if (state !== 'play') return;
 
@@ -134,33 +122,44 @@ function MultiplayerMemory() {
         // if two tiles are already flipped
         // or the clicked tile is already flipped
         // or the clicked tile is already matched
-        console.log(tileID)
-        console.log(flipped.length)
-        console.log(flipped.includes(tileID))
-        console.log(matched.includes(tileID))
-        if (flipped.length >= 2 || flipped.includes(tileID) || matched.includes(tileID)) {
+        if (flipped.length >= 2 || flipped.includes(tileID) || matched[tileID] !== 0) {
             return;
         }
-        console.log("here")
         if (flipped.length === 1) {
             // if two tiles have just been flipped
 
             if (tiles[flipped[0]] === tiles[tileID]) {
+
+                if (index === 1) {
+                    ws.send("reset");
+                }
                 // if the two tiles match
+                let temp = matched;
+                temp[flipped[0]] = index;
+                temp[tileID] = index;
 
                 // check if all tiles have been matched
-                if (matched.length + 2 === tiles.length) {
+                let finished = true;
+                temp.forEach((m) => {if (m === 0) {finished = false}})
+                if (finished) {
                     setState('won');
                 }
 
                 // add them to the doneTiles array
-                setMatched([...matched, flipped[0], tileID]);
+
+                setMatched(temp);
+                setTurn(0);
+                console.log("reset turn");
+            } else {
+                setTurn(turn+1);
             }
 
             // set a timer to hide them
             setTimeout(() => {
                 setFlipped([]);
             }, 1000);
+        } else {
+            setTurn(turn+1);
         }
 
         // flip the tile that was clicked
@@ -168,7 +167,6 @@ function MultiplayerMemory() {
     };
 
     function handleDifficulty(e: React.ChangeEvent<HTMLSelectElement>) {
-        setDifficulty(e.target.value);
         switch (e.target.value) {
             case 'easy':
                 setRows(3);
@@ -201,8 +199,11 @@ function MultiplayerMemory() {
     else if (cols === 8) grid_cols = 'grid-cols-8';
 
     function handleTileClick(index: number) {
-        handleTileFlip(index);
-        ws.send(index+"");
+        console.log(turn)
+        if (turn < 2) {
+            ws.send(index + "");
+            handleTileFlip(index, 1);
+        }
     }
 
     ws.onopen = () => {
@@ -212,30 +213,24 @@ function MultiplayerMemory() {
             // add message to end of msgs array,
             // re-assign to trigger UI update
             const message = await msg.data.text();
-            console.log(message);
             if (message.includes(",")) {
                 const cards = message.split(",");
                 setState('play');
-                console.log("here!!!")
-                console.log(cards.length)
                 if (cards.length === 12) {
                     setRows(3);
                     setCols(4);
-                    setDifficulty('easy');
+                    setMatched(Array<number>(12).fill(0));
                 } else if (cards.length === 20) {
                     setRows(4);
                     setCols(5);
-                    setDifficulty('medium');
+                    setMatched(Array<number>(20).fill(0));
                 } else {
                     setRows(5);
                     setCols(8);
-                    setDifficulty('hard');
+                    setMatched(Array<number>(40).fill(0));
                 }
-
                 setTiles(cards);
                 setFlipped([]);
-                setMatched([]);
-
                 setStartTime((new Date()).getTime());
             }
 
@@ -248,30 +243,24 @@ function MultiplayerMemory() {
                 // add message to end of msgs array,
                 // re-assign to trigger UI update
                 const message = await msg.data.text();
-                console.log(message);
                 if (message.includes(",")) {
                     const cards = message.split(",");
                     setState('play');
-                    console.log("here!!!")
-                    console.log(cards.length)
                     if (cards.length === 12) {
                         setRows(3);
                         setCols(4);
-                        setDifficulty('easy');
+                        setMatched(Array<number>(12).fill(0));
                     } else if (cards.length === 20) {
                         setRows(4);
                         setCols(5);
-                        setDifficulty('medium');
+                        setMatched(Array<number>(20).fill(0));
                     } else {
                         setRows(5);
                         setCols(8);
-                        setDifficulty('hard');
+                        setMatched(Array<number>(40).fill(0));
                     }
-
                     setTiles(cards);
                     setFlipped([]);
-                    setMatched([]);
-
                     setStartTime((new Date()).getTime());
                 }
 
@@ -282,12 +271,20 @@ function MultiplayerMemory() {
                 // re-assign to trigger UI update
                 const message = await msg.data.text();
                 console.log(message);
+                if (message === "reset") {
+                    setTurn(2);
+                }
                 if (!isNaN(parseInt(message))) {
-                    handleTileFlip(parseInt(message));
+                    handleTileFlip(parseInt(message), -1);
+                    if (turn === 2) {
+                        setTurn(3);
+                    } else {
+                        setTurn(0);
+                    }
                 }
             }
         }
-    }, [state, flipped, matched]);
+    }, [state, flipped, matched, turn]);
 
     return (
         <div className="h-screen flex flex-col justify-center items-center align-items-center text-center">
@@ -301,7 +298,7 @@ function MultiplayerMemory() {
                                 key={index}
                                 value={value}
                                 isFlipped={flipped.includes(index)}
-                                isDone={matched.includes(index)}
+                                isDone={matched[index]}
                                 onClick={() => handleTileClick(index)}
                             />
                         ))}
